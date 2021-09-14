1e33018f826382acd3ad3ce788242e09232206b2
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2021-09-14 20:29:40 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -5,6 +5,8 @@
     const opinionBack = document.getElementById("back");
     const opinion = document.getElementById("opinion");
     const before = document.getElementById("before");
+    const clientes = document.getElementById("clientes");
+    let array = document.getElementsByClassName("cliente-templates");
 
     let indiceMenu = 0;
     menu.addEventListener("click", () => {
@@ -22,12 +24,27 @@
         indiceMenu = 0;
     })
 
+    /*OPINIONES*/
+    let indiceOpiniones = -1;
     const scrollOpinionsNext = () => {
-        let currentScroll = opinion.scrollTop + 400;
+        /*let currentScroll = opinion.scrollTop + 400;
         opinion.scrollTo({
             top: currentScroll,
             behavior: "smooth",
-        });
+        });*/
+        if (indiceOpiniones < 3) {
+            indiceOpiniones += 1;
+            console.log(array[indiceOpiniones]);
+            const node = document.createElement("div");
+            node.setAttribute("class", "cliente");
+            console.log(indiceOpiniones);
+            node.innerHTML = array[indiceOpiniones].innerHTML;
+            clientes.innerHTML = "";
+            clientes.append(node);
+        }
+
+
+
         before.style.cssText = "background: rgba(0, 0, 0, 1);";
         setTimeout(() => {
             before.style.cssText = "background: rgba(0, 0, 0, 0.820);";
@@ -35,11 +52,24 @@
     }
 
     const scrollOpinionsBack = () => {
-        let currentScroll = opinion.scrollTop - 400;
+        /*let currentScroll = opinion.scrollTop - 400;
         opinion.scrollTo({
             top: currentScroll,
             behavior: "smooth",
-        });
+        });*/
+        if (indiceOpiniones > 0) {
+            indiceOpiniones -= 1;
+            console.log(array[indiceOpiniones]);
+            const node = document.createElement("div");
+            node.setAttribute("class", "cliente");
+            node.innerHTML = array[indiceOpiniones].innerHTML;
+            console.log(node);
+            clientes.innerHTML = "";
+            clientes.append(node);
+        }
+
+
+
         before.style.cssText = "background: rgba(0, 0, 0, 1);";
         setTimeout(() => {
             before.style.cssText = "background: rgba(0, 0, 0, 0.820);";
@@ -52,6 +82,6 @@
     opinionBack.addEventListener("click", () => {
         scrollOpinionsBack();
     })
-
+    scrollOpinionsNext();
 
 }())
