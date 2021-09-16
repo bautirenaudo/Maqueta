0c23c13e1af476497544d5309801b79567ef5f71
bautirenaudo <78118334+bautirenaudo@users.noreply.github.com>
2021-09-16 19:24:40 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -7,7 +7,10 @@
     const before = document.getElementById("before");
     const clientes = document.getElementById("clientes");
     let array = document.querySelector(".nada").content.querySelectorAll(".cliente-templates");
+    const headers = document.querySelectorAll("header");
+    const productos = document.querySelectorAll(".productos");
 
+    //MENU
     let indiceMenu = 0;
     menu.addEventListener("click", () => {
         if (indiceMenu == 0) {
@@ -24,20 +27,26 @@
         indiceMenu = 0;
     })
 
-    /*OPINIONES*/
+    const menuScroll = () => {
+        let currentScroll = window.scrollY;
+        if (currentScroll > 0) {
+            headers.forEach(header => {
+                header.style.cssText = "height: 80px; background-color: var(--color); box-shadow: 0 0 5px 2px black;";
+            })
+        } else {
+            headers.forEach(header => {
+                header.style.cssText = "height: 95px; background-color: none; box-shadow: none;";
+            })
+        }
+    }
+
+    //OPINIONES
     let indiceOpiniones = -1;
     const scrollOpinionsNext = () => {
-        /*let currentScroll = opinion.scrollTop + 400;
-        opinion.scrollTo({
-            top: currentScroll,
-            behavior: "smooth",
-        });*/
         if (indiceOpiniones < 3) {
             indiceOpiniones += 1;
-            console.log(array[indiceOpiniones]);
             const node = document.createElement("div");
             node.setAttribute("class", "cliente");
-            console.log(indiceOpiniones);
             node.innerHTML = array[indiceOpiniones].innerHTML;
             clientes.innerHTML = "";
             clientes.append(node);
@@ -52,18 +61,11 @@
     }
 
     const scrollOpinionsBack = () => {
-        /*let currentScroll = opinion.scrollTop - 400;
-        opinion.scrollTo({
-            top: currentScroll,
-            behavior: "smooth",
-        });*/
         if (indiceOpiniones > 0) {
             indiceOpiniones -= 1;
-            console.log(array[indiceOpiniones]);
             const node = document.createElement("div");
             node.setAttribute("class", "cliente");
             node.innerHTML = array[indiceOpiniones].innerHTML;
-            console.log(node);
             clientes.innerHTML = "";
             clientes.append(node);
         }
@@ -84,4 +86,25 @@
     })
     scrollOpinionsNext();
 
+
+    //MOSTAR ELEMNTOS
+    const mostarElementos = () => {
+        let currentScroll = window.scrollY;
+        if (currentScroll > 900) {
+            productos.forEach(producto => {
+                producto.style.opacity = "1";
+                producto.style.cssText = "transition: all .5s;";
+            })
+        } else if (currentScroll < 400) {
+            productos.forEach(producto => {
+                producto.style.opacity = "0";
+            })
+        }
+
+    }
+
+    window.addEventListener("scroll", () => {
+        menuScroll();
+        mostarElementos();
+    })
 }())
